90ca5b1c3f3952aa417b57bbe6b81bd6979a9aef
kevinjang <kevinjang@163.com>
2019-10-29 17:29:27 +0800

    Update query.js
    新增异常响应，防止光连不断的情况。

--- BLL/query.js
+++ BLL/query.js
@@ -3,6 +3,10 @@ const request = require('./getRequest');
 // var result = 'x';
 async function query(sql){
     const p = await pool();
+    p.on('error',err=>{
+        console.log('pool-error:', err);
+        p.close();
+    })
     const result = await request(p).query(sql);
     p.close();
     return result && result.recordsets? result.recordsets: {};;
